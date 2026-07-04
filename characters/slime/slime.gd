92cb5b7a3930b20da25bcc7f1a3c44138bd64c5d
ProjectsOfJames <whjdewet@gmail.com>
2026-07-04 14:00:06 +0200

    Cleanup & Mob variation
    - Removed foreign assets
    - Added mob colour variation

--- characters/slime/slime.gd
+++ characters/slime/slime.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 const rebound = 20
 
+func _ready() -> void:
+	randomize()
+	var body_colour = Color(randf(), randf(), randf())
+	$Anchor/SlimeBody.modulate = body_colour.lightened(0.6)
+	$Anchor/Face/SlimeFace.modulate = body_colour.lightened(0.9)
+	
 func play_walk():
 	%AnimationPlayer.play("walk")
 
